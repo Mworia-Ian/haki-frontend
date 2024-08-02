86881a7021afabff509ba391c2b3b61481cb0e3b
Denis <mugambimuthuri0@gmail.com>
2024-08-02 20:06:45 +0300

    "Changed route path from '/' to '/login' for LoginPage element"

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ export default function App() {
     <Router>
       <Routes>
         
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
         
       </Routes>
     </Router>
