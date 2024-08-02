7937416a6e44c1079d7f2ecb4fbc1d1fb5e85eff
Leslie Mwangi <luciannahphoi@gmail.com>
2024-08-02 23:39:45 +0300

    navbar style

--- src/components/LawyersNavbar.jsx
+++ src/components/LawyersNavbar.jsx
@@ -3,7 +3,7 @@ import { Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faGavel, faUser, faBars, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
-import '../css/Lawyers.css';
+import '../css/Lawyers.css'; // Ensure this CSS file is created and properly linked
 
 const LawyersNavbar = () => {
   const [isCollapsed, setIsCollapsed] = useState(false);
@@ -20,7 +20,7 @@ const LawyersNavbar = () => {
       >
         <FontAwesomeIcon icon={faBars} />
       </button>
-      <Nav className="flex-column p-3">
+      <Nav className="flex-column p-3 flex-grow-1">
         <Nav.Link as={Link} to="/home">
           <FontAwesomeIcon icon={faHome} className="fa-icon" />
           {!isCollapsed && 'Home'}
@@ -29,10 +29,10 @@ const LawyersNavbar = () => {
           <FontAwesomeIcon icon={faGavel} className="fa-icon" />
           {!isCollapsed && 'Lawyers'}
         </Nav.Link>
-        {/* <Nav.Link as={Link} to="/profile">
+        <Nav.Link as={Link} to="/profile">
           <FontAwesomeIcon icon={faUser} className="fa-icon" />
           {!isCollapsed && 'Profile'}
-        </Nav.Link> */}
+        </Nav.Link>
       </Nav>
       <Nav className="flex-column p-3 mt-auto">
         <Nav.Link as={Link} to="/logout">
