8c4a03ee1e6ad4498c132f2c10a7eb3e2aef5250
Denis <mugambimuthuri0@gmail.com>
2024-08-02 21:16:02 +0300

    "Added Facebook social button to login page"

--- src/pages/Login/LoginPage.jsx
+++ src/pages/Login/LoginPage.jsx
@@ -104,6 +104,11 @@ export default function LoginPage() {
               <path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"></path>
             </svg>
           </Button>
+          <Button className="social-button facebook">
+            <svg className="svg" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 24 24">
+              <path d="M12 0C5.373 0 0 5.373 0 12c0 6.016 4.432 10.984 10.207 11.852V15.18h-2.97v-3.155h2.97V9.927c0-3.475 1.693-5 4.58-5c1.384 0 2.115.102 2.462.149v2.753h-1.97c-1.226 0-1.655 1.163-1.655 2.473v1.724h3.594l-.488 3.155h-3.106v8.696C19.481 23.083 24 18.075 24 12c0-6.627-5.373-12-12-12"></path>
+              </svg>
+          </Button>
         </div>
       </div>
       <span className="agreement">
