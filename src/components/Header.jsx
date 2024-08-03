b0350b491e3c605a0c408946f7645fe312694b8c
Denis <mugambimuthuri0@gmail.com>
2024-08-03 20:14:38 +0300

    "Updated navigation links in Header component to use absolute paths"

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,11 +9,11 @@ const Header = () => {
                 <img src="images/menu.svg" alt="Menu" />
             </div>
             <nav className="navbar">
-                <a href="#">home</a>
-                <a href="#">about</a>
-                <a href="#">services</a>
-                <a href="#">reviews</a>
-                <a href="#">contact</a>
+                <a href="/">home</a>
+                <a href="/about">about</a>
+                <a href="/services">services</a>
+                <a href="/reviews">reviews</a>
+                <a href="/contact">contact</a>
             </nav>
             <a href="/login" className="btn">LOG IN</a>
             <a href="#" className="btn">SIGN UP</a>
