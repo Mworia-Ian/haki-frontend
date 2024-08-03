31e600df7a2bcc9cbbc6db282b903cee1db01cdd
Denis <mugambimuthuri0@gmail.com>
2024-08-03 20:07:42 +0300

    "Updated CSS styles for ReviewsSection and ServicesSection components, and refactored ServicesSection JSX to use an array of services and map over it to render boxes."

--- src/components/ServicesSection.jsx
+++ src/components/ServicesSection.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import arrowUpImg from '../assets/arrowUpImg.png';
 import './ServicesSection.css';
 
-
 const ServicesSection = () => {
+    const services = [
+        { id: 1, name: 'Criminal law' },
+        { id: 2, name: 'Family law' },
+        { id: 3, name: 'Tax law' },
+        { id: 4, name: 'Administrative law' },
+        { id: 5, name: 'Corporate law' },
+        { id: 6, name: 'Intellectual Property law' },
+        { id: 7, name: 'Environment law' },
+        { id: 8, name: 'Labor and Employment law' },
+        { id: 8, name: 'Real Estate law' }
+    ];
+
     return (
         <section className="services">
             <div className="heading">
@@ -10,21 +22,16 @@ const ServicesSection = () => {
                 <h3>Our lawyers specialize in</h3>
             </div>
             <div className="box-container">
-                <div className="box">
-                    <h1><span>C</span>riminal law</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, quasi?</p>
-                    <a href="#">Learn more <img src="src/assets/arrowUpImg.png" alt="Arrow" style={{ width: '.8rem', marginLeft: '5px' }} /></a>
-                </div>
-                <div className="box">
-                    <h1><span>F</span>amily law</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, quasi?</p>
-                    <a href="#">Learn more <img src="src/assets/arrowUpImg.png" alt="Arrow" style={{ width: '.8rem', marginLeft: '5px' }} /></a>
-                </div>
-                <div className="box">
-                    <h1><span>T</span>ax law</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, quasi?</p>
-                    <a href="#">Learn more <img src="src/assets/arrowUpImg.png" alt="Arrow" style={{ width: '.8rem', marginLeft: '5px' }} /></a>
-                </div>
+                {services.map(service => (
+                    <div className="box" key={service.id}>
+                        <h1><span>{service.name[0]}</span>{service.name.slice(1)}</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, quasi?</p>
+                        <a href="#">
+                            Learn more 
+                            <img src={arrowUpImg} alt="Arrow" style={{ width: '.8rem', marginLeft: '5px' }} />
+                        </a>
+                    </div>
+                ))}
             </div>
         </section>
     );
